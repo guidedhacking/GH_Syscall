b5548c067379d04ea39712d4dbf954cf9da319d7
Traxin <traxin@noreply.github.com>
2019-03-07 14:26:56 -0500

    added the holy grail of definitions

--- GH_Syscall/Source.cpp
+++ GH_Syscall/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "gh_syscall.h"
+#include "nt_defs.h"
+#include "syscall_defs.h"
 
 using namespace std;
 
@@ -9,10 +11,25 @@ int main()
 	SIZE_T retLen = 0;
 	ScQueryVirtualMemory( HANDLE( -1 ), (PVOID) Sleep, MemoryBasicInformation, (PVOID) &mbi, sizeof( mbi ), &retLen );
 
+	HANDLE hProc = nullptr;
+	OBJECT_ATTRIBUTES oa{ 0 };
+	oa.Length = sizeof( oa );
+	CLIENT_ID ci{ 0 };
+	DWORD dwPID = GetCurrentProcessId();
+	ci.UniqueProcess = (void*)dwPID;
+	NTSTATUS ntStatus = ScOpenProcess(&hProc, PROCESS_ALL_ACCESS, &oa, &ci);
+
+	if ( ntStatus )
+	{
+		cout << "Failed to open process..." << endl;
+	}
+
 	if ( retLen )
 	{
 		cout << "Sleep is on page 0x" << mbi.BaseAddress << endl;
 	}
+
+	ntStatus = ScClose( hProc );
 
 	getchar();
 
--- GH_Syscall/gh_syscall.h
+++ GH_Syscall/gh_syscall.h
@@ -2,16 +2,11 @@
 #include <Windows.h>
 #include <functional>
 #include <vector>
+#include "nt_defs.h"
 
 // so you create your all your defs and all that jazz
 // and then jump to the bottom of this file and create
 // "ezCall" (tm) defines to one line these bad boys
-
-typedef enum _MEMORY_INFORMATION_CLASS {
-	MemoryBasicInformation
-} MEMORY_INFORMATION_CLASS;
-
-using fnNtQueryVirtualMemory = NTSTATUS( NTAPI* )(HANDLE ProcessHandle, PVOID BaseAddress, MEMORY_INFORMATION_CLASS MemoryInformationClass, PVOID MemoryInformation, SIZE_T MemoryInformationLength, PSIZE_T ReturnLength);
 
 class TSyscall
 {
@@ -30,7 +25,8 @@ std::function<T> TSyscall::GetInvoke( const char* sFunction, T * pAddress )
 		pCodeLoc = VirtualAlloc( nullptr, 0x1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE ); // if you really want to be sneaky you can scan for a code cave
 #ifdef _WIN64
 		BYTE cb[] = { 0x4C, 0x8B, 0xD1, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x05, 0xC3 };
-		memcpy( pCodeLoc, cb, ARRAYSIZE( cb ) );
+		auto size = ARRAYSIZE( cb );
+		memcpy( pCodeLoc, cb, size);
 #endif
 	}
 
@@ -45,5 +41,4 @@ std::function<T> TSyscall::GetInvoke( const char* sFunction, T * pAddress )
 
 #define _sc(t, s) TSyscall::GetInvoke(s, (t)nullptr)
 
-// use the define above to create an ezCall (tm) define like below.
-#define ScQueryVirtualMemory _sc(fnNtQueryVirtualMemory, "NtQueryVirtualMemory")
+#include "syscall_defs.h"
